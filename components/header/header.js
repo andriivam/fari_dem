24def3f5a90bc7ff5312bc97f06b68c1e0531718
andriivam <andriivlasiuk2@gmail.com>
2023-05-23 11:18:23 +0200

    change style

--- components/header/header.js
+++ components/header/header.js
@@ -8,7 +8,10 @@ const Header = () => {
             <div className={styles.btnWrapper}>
                 <button className={styles.backHomeBtn}><Image className={styles.icon} src="/static/arrow.svg" alt="arrow" width={24} height={24} />Back to homepage</button>
             </div>
-            <div className={styles.btnWrapper}><button className={styles.helpBtn}>Help</button></div>
+            <div className={styles.btnWrapper}>
+                <button className={styles.languageBtn}>EN<Image className={styles.icon} src="/static/language.svg" width={24} height={24} /></button>
+                <button className={styles.helpBtn}>Help <Image className={styles.icon} src="/static/question.svg" width={24} height={24} /></button>
+            </div>
         </div>
     )
 }
--- components/imageInput/image-input.js
+++ components/imageInput/image-input.js
@@ -4,15 +4,12 @@ import Image from 'next/image';
 const ImageInput = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.content}>
-                <div className={styles.imageContainer}>
-                    <Image className={styles.image} src="https://plus.unsplash.com/premium_photo-1664303877925-2a582c551c0f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2369&q=80" alt="Fox in the show" width={450} height={450} />
-                </div>
-                <div className={styles.btnDiv}>
-                    <button className={styles.btn}>Image</button>
-                </div>
+            <div className={styles.imageContainer}>
+                <Image className={styles.image} src="https://images.unsplash.com/photo-1578170681526-de18d598a5b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2338&q=80" alt="Fox in the show" width={450} height={450} />
             </div>
-
+            <div className={styles.textDiv}>
+                <p className={styles.imageParagraph}>Image</p>
+            </div>
         </div>
     )
 }
